e59fc19f025141400f4b0af3b6c6535b10c34ff3
mahdi <mallakimahdi@gmail.com>
2023-05-31 13:47:42 +0330

    first commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    Random random = new Random();
+
+    private RestTemplate restTemplate;
+    @Value("${spring.application.name}")
+    private String applicationName;
+    public Controller(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    @GetMapping("/path1")
+    public ResponseEntity<String> path1() {
+        logger.info("Incoming request at {} for request /path1 ", applicationName);
+        doNothingButSleepForSomeTime();
+        String response = restTemplate.getForObject("http://localhost:8090/hello/path2", String.class);
+        doNothingButSleepForSomeTime();
+        return ResponseEntity.ok("response from /path1 + " + response);
+    }
+
+    @GetMapping("/path2")
+    public ResponseEntity<String>  path2() {
+        logger.info("Incoming request at {} at /path2", applicationName);
+        doNothingButSleepForSomeTime();
+        return ResponseEntity.ok("response from /path2 ");
+    }
+
+
+    public void doNothingButSleepForSomeTime() {
+        try {
+            int sleepTime = random.nextInt(2, 5);
+            logger.info("sleeping for " + sleepTime + " seconds");
+            Thread.sleep(sleepTime * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Configuration
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
+	}
+}
